4f4cf4219023280037ce75882c4e7615fcfd889a
Travis H <t3patterson@gmail.com>
2014-10-08 08:59:40 -0500

    project complete with click event listener

--- app.js
+++ app.js
@@ -28,67 +28,90 @@ console.log('hey there')
  * 
  */
 
-Array.prototype.contains = function (needle) {
-   for (i in this) {
-       if (this[i] == needle) return true;
-   }
-   return false;
+
+
+Array.prototype.contains = function (locateMe) {
+	for (i in this) {
+	    if (this[i] == locateMe) return true;
+	}
+	return false;
 }
 
-var a = 4
+//listens for 'click' event and toggles clickStatus to on/off
+var clickStatus = 'off'
+document.querySelector('.timer-box').addEventListener("click", 
+ 		function(){
+ 			 clickStatus == 'on' ? 
+ 			 	clickStatus ='off' :
+ 				clickStatus = 'on'		
+ 		})
 
-
-//specify a function so that it runs every second
+// interval function to get the time and display every second
 setInterval(function(){
-var d = new Date
+
+	var d = new Date
+
+	//captures the hours/minutes/seconds in one object
+	var currentTime = new Object
+	currentTime.hour = d.getHours()
+	currentTime.minute = d.getMinutes()
+	currentTime.second = d.getSeconds()
+
+	//maps second to alphabet
+	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var secondsToAlphabet = alphabet[currentTime.second % 26]
+	var showLetter = "0"+secondsToAlphabet
 
 
 
+	//line 66-82: puts the hours/minutes/seconds-or-Letters into the html
+	document.querySelector('.hour').innerHTML =
+		 (currentTime.hour < 10 ? "0" + currentTime.hour : currentTime.hour)
 
-//captures the hours/minutes/seconds
-var currentTime = new Object
-currentTime.hour = d.getHours()
-currentTime.minute = d.getMinutes()
-currentTime.second = d.getSeconds()
+	document.querySelector('.minute').innerHTML =
+		(currentTime.minute < 10 ? "0" + currentTime.minute : currentTime.minute)
 
-//puts the hours/minutes/seconds into the html
-document.querySelector('.hour').innerHTML =
-	(currentTime.hour < 10 ? "0" + currentTime.hour : currentTime.hour)
+		 //puts seconds or letters, depending on clickStatus
+		switch (clickStatus)
+			{
+			case "on": document.querySelector('.second').innerHTML = 
+				showLetter
+				break
+			default: 
+				document.querySelector('.second').innerHTML = 
+					(currentTime.second < 10 ? "0" + currentTime.second : currentTime.second)
+				break
+			}
+	
+	//monitors seconds to determine border-div width - gives effect of growing border
+	document.querySelector('.line').style.width = (currentTime.second/60*100+"%")
 
-document.querySelector('.minute').innerHTML =
-	(currentTime.minute < 10 ? "0" + currentTime.minute : currentTime.minute)
+	//the background change effect
+	//	- colorTimes determines the timing of color change
+	//	- bgColors determines the color
+	var colorTimes = [
+		10,
+		20,
+		30,
+		40,
+		50,
+		00
+		]
 
-document.querySelector('.second').innerHTML = 
-	(currentTime.second < 10 ? "0" + currentTime.second : currentTime.second)
-
-//growing-border second meter 
-document.querySelector('.line').style.width = (currentTime.second/60*100+"%")
-
-
-//programming the background changes
-var colorTimes = [
-	10,
-	20,
-	30,
-	40,
-	50,
-	00
+	var bgColors = [
+		"133,133,0",
+		"75,133,88",
+		"99,100,175",
+		"100,75,60",
+		"11,33,145",
+		"171,11,0"
 	]
 
-var bgColors = [
-	"133,133,0",
-	"75,133,88",
-	"99,100,175",
-	"100,75,60",
-	"11,33,145",
-	"171,11,0"
-]
+	if(colorTimes.contains(currentTime.second)){
+		var bgIndex = [colorTimes.indexOf(currentTime.second)]
+		document.querySelector('.container').style.background = "rgb("+bgColors[bgIndex]+")"
 
-if(colorTimes.contains(currentTime.second)){
-	var bgIndex = [colorTimes.indexOf(currentTime.second)]
-	document.querySelector('.container').style.background = "rgb("+bgColors[bgIndex]+")"
-
-	} 
+		} 
 
 
 },1000)
